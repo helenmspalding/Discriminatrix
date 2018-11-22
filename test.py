de8ac2d16e0ec533b7ad80e8ffdd670f57e4143b
sercwy <sercwy@gmail.com>
2018-11-22 09:57:15 +0000

    Renamed CSV file, python code using pandas to read file

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-hello
-yes
+# hello
+# yes
+
+import pandas as pd
+import numpy as np
+
+dataset = pd.read_csv('Gender_pay_gap.csv')#, delimiter=';', names = ['my_datetime', 'event', 'country', 'user_id', 'source', 'topic'])
+print dataset.to_json()
+# print dataset
