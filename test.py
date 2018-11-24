7f2bafd1ef3b55c14a1cb562b569561d9f8053e9
sercwy <sercwy@gmail.com>
2018-11-24 16:35:22 +0000

    Reads Gender pay gap csv

--- test.py
+++ test.py
@@ -5,5 +5,5 @@ import pandas as pd
 import numpy as np
 
 dataset = pd.read_csv('Gender_pay_gap.csv')#, delimiter=';', names = ['my_datetime', 'event', 'country', 'user_id', 'source', 'topic'])
-print dataset.to_json()
-# print dataset
+# print dataset.to_json()
+print dataset
