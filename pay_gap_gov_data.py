5fb705c53e7157c171b18025ae8fa942121e540b
helenmspalding <32137188+helenmspalding@users.noreply.github.com>
2018-11-28 19:34:34 +0000

    company name function and Flask company name

--- pay_gap_gov_data.py
+++ pay_gap_gov_data.py
@@ -1,6 +1,32 @@
 import pandas as pd
 import numpy as np
+from flask import Flask
+app = Flask(__name__)
+
 
 dataset = pd.read_csv('Gender_pay_gap.csv')
-print dataset
 # print dataset
+
+#setting preview of top ten csv lines for ease
+dataset = pd.read_csv('Gender_pay_gap.csv')
+dataset = dataset.head(10)
+
+# function to return company name
+employernames = dataset["EmployerName"]
+employernames = employernames.str.replace(" ", "_", regex=True)
+
+dataset.index = employernames
+print dataset
+
+@app.route('/<name>/')
+def name(name):
+    # company_name = raw_input("What company are you interested in? ")
+    location = dataset.loc[name]
+    return str(location)
+
+# name(company_name)
+#
+app.run(debug=True)
+
+
+
